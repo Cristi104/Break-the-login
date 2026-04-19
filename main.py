57e65a408a698965c6f8fe0ad629913fb5859ba0
cristi <petrecristian2004@gmail.com>
2026-04-19 17:13:04 +0300

    web access

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import src.db
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 db = src.db.Database()
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/login", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request, "login.html", {"request": request})
+
+@app.get("/register", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request, "register.html", {"request": request})
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request, "index.html", {"request": request})
+
